66fe13d9de8b55037c9e3fc12905728cf67196ce
axeoppal-ops <axeoppal@gmail.com>
2026-05-09 16:02:28 +0300

    Update vite.netlify.config.ts

--- vite.netlify.config.ts
+++ vite.netlify.config.ts
@@ -15,4 +15,8 @@ export default defineConfig({
     netlify(),
     viteReact(),
   ],
+  define: {
+    'import.meta.env.VITE_SUPABASE_URL': JSON.stringify(process.env.VITE_SUPABASE_URL),
+    'import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY': JSON.stringify(process.env.VITE_SUPABASE_PUBLISHABLE_KEY),
+  },
 });
