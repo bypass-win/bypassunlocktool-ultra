9f27a4b0ca337025ffcfd05c0d7ba189a1d779eb
axeoppal-ops <axeoppal@gmail.com>
2026-05-09 19:55:01 +0300

    Update settings.ts

--- src/lib/settings.ts
+++ src/lib/settings.ts
@@ -4,7 +4,7 @@ import { supabase } from "@/integrations/supabase/client";
 export type Settings = Record<string, string>;
 
 const DEFAULTS: Settings = {
-  download_url_windows: "https://mega.nz/file/IypkXQwQ#3tZTniXjKPN6BrB5DlT1sUxbdone-q_LYY8tA6rNna8",
+  download_url_windows: "https://mega.nz/folder/a1hVwDxL#xsXIa7miRPDHdEIdxuyQ1w",
   payments_enabled: "true",
   telegram_url: "https://t.me/BYPASS_UNLOCK_TOOL",
   contact_email: "BypassUnlocktool@outlook.com",
